571093a75eb68557e9350579ba99c84e38bc724f
albert.oclarit <bbjao143213620>
2016-09-02 08:44:37 +0800

    ZKemKeeper start

---
+++ src/main/java/com/hisd3/dtr/web/rest/BundyClockResource.java
@@ -0,0 +1,20 @@
+package com.hisd3.dtr.web.rest;
+
+import com.hisd3.dtr.zkemkeeper.ZKemKeeperService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+
+/**
+ * Created by albertoclarit on 9/2/16.
+ */
+@RestController
+@RequestMapping("/api/bundyclock")
+public class BundyClockResource {
+
+
+
+
+
+}
---
+++ src/main/java/com/hisd3/dtr/zkemkeeper/ZKemKeeperService.java
@@ -0,0 +1,19 @@
+package com.hisd3.dtr.zkemkeeper;
+
+/**
+ * Created by albertoclarit on 9/2/16.
+ */
+
+public class ZKemKeeperService {
+
+    private String ip;
+    private int port;
+    ZKemKeeperService(String ip,int port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+
+
+
+}

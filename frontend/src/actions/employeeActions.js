1655a1bf556d5fe798bba155ad656016149fa8dd
kn4rfy <kn4rfy@gmail.com>
2016-09-10 21:37:21 +0800

    no message

--- frontend/src/actions/employeeActions.js
+++ frontend/src/actions/employeeActions.js
@@ -4,25 +4,24 @@ import {post, get} from '~/src/utils/RestClient';
 
 export let getEmployeeActions = () =>{
 
-    return dispatch =>{
-        get('api/bundyclock/getusers').then((response) =>{
+  return dispatch =>{
+    get('api/bundyclock/getusers').then((response) =>{
 
-            var records = response.data;
+      var records = response.data;
 
-            dispatch(getEmployeeActionsSuccess(records));
+      dispatch(getEmployeeActionsSuccess(records));
 
-        });
-    };
+    });
+  };
 }
 
 
 
 export let getEmployeeActionsSuccess=(records)=>{
 
-            return {
-                    type:types.LOAD_EMPLOYEE,
-                    records
-            }
+  return {
+    type:types.LOAD_EMPLOYEE,
+    records
+  }
 
 }
-
